7f43e8194c991ec2908b138053b45e2ebade32cb
erischon <66017574+erischon@users.noreply.github.com>
2023-11-23 12:33:56 +0100

    layout

---
+++ src/app/layout.jsx
@@ -0,0 +1,22 @@
+import Header from "@/components/header";
+import "./globals.css";
+import Footer from "@/components/footer";
+
+export const metadata = {
+  title: "Noob Hacker",
+  description: "A blog about hacking and programming",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="flex flex-col min-h-screen bg-slate-900 text-slate-300">
+        <Header />
+
+        <main className="flex-1">{children}</main>
+
+        <Footer />
+      </body>
+    </html>
+  );
+}
